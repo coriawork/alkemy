b413fed07ee64216bde4ca0ffe8f32b6ec787d8d
coriawork <manuelcoriawork@gmail.com>
2026-07-27 18:52:58 -0300

    Agregar modelos Categoria y Producto, y actualizar index.php para la gestión de usuarios y carritos

--- public/index.php
+++ public/index.php
@@ -2,3 +2,14 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+require_once __DIR__ . '/../core/Db.php';
+require_once __DIR__ . '/../src/models/Modelo.php';
+require_once __DIR__ . '/../src/models/Usuario.php';
+require_once __DIR__ . '/../src/models/Carrito.php';
+
+$usuarioModel = new App\Models\Usuario();
+$usuario = $usuarioModel->get(1);
+$carrito = $usuarioModel->carrito($usuario);
+
+var_dump($usuario);
+var_dump($carrito);
