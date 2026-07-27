fc562967a0565adff092f6d6c01ef56a4a7b5c94
coriawork <manuelcoriawork@gmail.com>
2026-07-27 19:13:56 -0300

    Actualizar index.php para incluir la gestión de productos en el carrito y eliminar importación innecesaria en Usuario.php

--- public/index.php
+++ public/index.php
@@ -4,12 +4,23 @@ require __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../core/Db.php';
 require_once __DIR__ . '/../src/models/Modelo.php';
+require_once __DIR__ . '/../src/models/Modelo.php';
 require_once __DIR__ . '/../src/models/Usuario.php';
 require_once __DIR__ . '/../src/models/Carrito.php';
+require_once __DIR__ . '/../src/models/Producto.php';
 
 $usuarioModel = new App\Models\Usuario();
+$carritoModel = new App\Models\Carrito();
+
 $usuario = $usuarioModel->get(1);
 $carrito = $usuarioModel->carrito($usuario);
+$productos = $carritoModel->productos($carrito);
 
+echo '<h3>Usuario</h3>';
 var_dump($usuario);
+
+echo '<h3>Carrito</h3>';
 var_dump($carrito);
+
+echo '<h3>Productos del carrito</h3>';
+var_dump($productos);
