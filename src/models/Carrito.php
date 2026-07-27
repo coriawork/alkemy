3133b32bf47c760be9d02a91e86acd3139ad83ba
coriawork <manuelcoriawork@gmail.com>
2026-07-27 19:13:26 -0300

    Agregar método productos en la clase Carrito para obtener productos del carrito

--- src/models/Carrito.php
+++ src/models/Carrito.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Core\Db;
+
 class Carrito extends Modelo {
     private $campos = [
         'id',
@@ -15,4 +17,23 @@ class Carrito extends Modelo {
         parent::__construct($this->tablaName, $this->campos);
     }
 
+    function productos($carrito){
+        //chekeo que el carrito recibido sea un array y que tenga la clave 'productos'
+        if (!is_array($carrito) || !isset($carrito['productos'])) {
+            return [];
+        }
+
+        //obtengo la tabla productos
+        $tablaProductos = Db::getTable('productos');
+        $productos = [];
+
+        //recorro todos los ids de productos del carrito y busco en la tabla de productos para quedarme con los productos que correspondan a esos ids
+        foreach ($carrito['productos'] as $productoId) {
+            if (isset($tablaProductos[$productoId])) {
+                $productos[] = $tablaProductos[$productoId];
+            }
+        }
+
+        return $productos;
+    }
 }
